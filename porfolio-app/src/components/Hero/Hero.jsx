7c1bfe685f114eb06ac93883833f489c57546271
Drake224025 <sasithamare@gmail.com>
2024-04-04 14:07:17 +0530

    feat: Complete hero section

---
+++ porfolio-app/src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/no-unescaped-entities */
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
+
+function Hero() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Sasitha</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 3 years of experience using React and
+          NodeJS. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+        <img
+          src={getImageUrl("hero/heroImage.png")}
+          alt="Hero image of me"
+          className={styles.heroImg}
+        />
+        {/* <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} /> */}
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
